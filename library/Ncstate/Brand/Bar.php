17c1b45ad51ddb791896675d932b1b501cd89156
Jason Austin <jfaustin@gmail.com>
2011-03-24 11:02:04 -0400

    Upgraded to version 1.0.0 of PHP_Ncstate

--- library/Ncstate/Brand/Bar.php
+++ library/Ncstate/Brand/Bar.php
@@ -2,8 +2,8 @@
 /**
  * Set of classes to programatically create certain aspects of the
  * approved brand at NC State University
- *
- * @package Ncstate_Brand
+ * 
+ * @package Ncstate_Brand 
  * @see     http://ncsu.edu/brand
  * @author  Office of Information Technology - Outreach Technology
  */
@@ -11,28 +11,28 @@
 /**
  * Class to generate the HTML code for the NC State Branding bar which is
  * required on all NC State public web pages.
- *
+ * 
  */
 final class Ncstate_Brand_Bar
 {
     /**
      * URL to the style sheet
-     *
+     * 
      * @var string
      */
     const STYLESHEET_URL = 'http://www.ncsu.edu/brand/utility-bar/iframe/css/utility_bar_iframe.css';
-
+    
     /**
-     *
+     * 
      * Base URL for the Iframe which is injected into the top of the web page
-     *
+     * 
      * @var string
      */
     const IFRAME_URL = 'http://www.ncsu.edu/brand/utility-bar/iframe/index.php';
-
+    
     /**
      * Options available for configuring the brand bar.
-     *
+     * 
      * @var array
      */
     protected $_options = array(
@@ -42,10 +42,10 @@ final class Ncstate_Brand_Bar
         'noIframePrompt' => 'Your browser does not support inline frames or is currently configured  not to display inline frames.<br /> Visit <a href="http://ncsu.edu/">http://www.ncsu.edu</a>.',  // Prompt provided to users who have browsers that don't support iframes
         'iframeId'       => 'ncsu_branding_bar',  // HTML id for the branding bar iframe
     );
-
+    
     /**
      * Available color options for the branding bar
-     *
+     * 
      * @var array
      */
     protected $_colorOptions = array(
@@ -54,20 +54,20 @@ final class Ncstate_Brand_Bar
         'red_on_white'   => 'Red Text, White Background',
         'black_on_white' => 'Black Text, White Background',
     );
-
+    
     /**
      * Constructor
-     *
+     * 
      * @param array $options
      */
     public function __construct(array $options = array())
     {
         $this->setOptions($options);
     }
-
+    
     /**
      * Sets the options for the branding bar
-     *
+     * 
      * @param array $options
      * @return object for fluent interface
      */
@@ -78,18 +78,18 @@ final class Ncstate_Brand_Bar
                 $this->_options[$key] = $value;
             }
         }
-
+        
         return $this;
     }
-
+    
     /**
      * Gets the options for the branding bar
      */
     public function getOptions()
     {
         return $this->_options;
-    }
-
+    }    
+    
     /**
      * Gets the color options for the branding bar
      */
@@ -97,53 +97,53 @@ final class Ncstate_Brand_Bar
     {
         return $this->_colorOptions;
     }
-
+    
     /**
      * Gets the full HTML of the branding bar, including stylesheet and iframe code
-     *
+     * 
      * @return string
      */
     public function getBarHtml()
     {
         return "\n" . $this->getStylesheetHtml() . "\n" . $this->getIframeHtml() . "\n";
     }
-
+    
     /**
      * Gets the HTML for the style sheet
-     *
+     * 
      * @return string
      */
     public function getStylesheetHtml()
     {
         return '<link rel="stylesheet" type="text/css" href="' . $this->getStylesheetUrl() . '" media="screen" />';
     }
-
+    
     /**
      * Gets the URL for the style sheet
-     *
+     * 
      * @return string
      */
     public function getStylesheetUrl()
     {
         return self::STYLESHEET_URL;
     }
-
+    
     /**
      * Gets the HTML for the iframe as set by the options
-     *
+     * 
      * @return string
      */
     public function getIframeHtml()
     {
-        return '<iframe name="' . $this->_options['iframeId'] . '" id="'
+        return '<iframe name="' . $this->_options['iframeId'] . '" id="' 
              . $this->_options['iframeId'] . '" frameborder="0" src="' . $this->getIframeUrl() . '" scrolling="no">'
              . $this->_options['noIframePrompt']
              . '</iframe>';
     }
-
+    
     /**
      * Gets the configured URL for the branding bar that the iframe occupies
-     *
+     * 
      * @return string
      */
     public function getIframeUrl()
@@ -151,13 +151,13 @@ final class Ncstate_Brand_Bar
         if (!isset($this->_colorOptions[$this->_options['color']])) {
             $this->_options['color'] = 'red';
         }
-
+        
         if (preg_match('/^http(s)*:\/\//i', $this->_options['siteUrl'])) {
             $this->_options['siteUrl'] = preg_replace('/^http(s)*:\/\//i', '', $this->_options['siteUrl']);
         }
-
-        return self::IFRAME_URL . '?color=' . urlencode($this->_options['color'])
-             . '&inurl=' . urlencode($this->_options['siteUrl']) . '&center='
+        
+        return self::IFRAME_URL . '?color=' . urlencode($this->_options['color']) 
+             . '&inurl=' . urlencode($this->_options['siteUrl']) . '&center=' 
              . urlencode((($this->_options['centered']) ? 'yes' : 'no'));
     }
 }
